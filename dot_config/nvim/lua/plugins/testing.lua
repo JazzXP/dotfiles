fac225a7f8b4f9bcb623aea2089b93650bf16aea
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-11-20 09:51:48 +1100

    ViTest coverage

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     adapters = {
       ["neotest-vitest"] = {
+        vitestCommand = "npx vitest run --coverage",
         filter_dir = function(name, rel_path, root)
           return name ~= "node_modules"
         end,
