da89add60a766ec4ecc1823519ca91e5ac003aef
Sam Dickinson <sam@sdickinson.com>
2024-11-29 07:09:29 +1100

    Added codestats to lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,22 @@
+local codestats = require("codestats")
+local xp = function()
+  return codestats.get_xp(0) .. "/" .. codestats.get_xp() -- current buf language xp
+end
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       opts.sections.lualine_z = {}
+      opts.sections.lualine_z = {
+        "location",
+        "filetype",
+        {
+          xp,
+          fmt = function(s)
+            return s and (s ~= "0/0" or nil) and s .. "xp"
+          end,
+        },
+      }
     end,
   },
 }
