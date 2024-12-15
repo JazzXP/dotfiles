82a3d9c9118b507a6964fc4e1ed5c4635e2df875
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-12-16 09:40:41 +1100

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/lspconfig.lua Update .zshrc

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,18 +1,48 @@
+local on_publish_diagnostics = vim.lsp.diagnostic.on_publish_diagnostics
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local on_publish_diagnostics = vim.lsp.diagnostic.on_publish_diagnostics
-      opts.servers.bashls = vim.tbl_deep_extend("force", opts.servers.bashls or {}, {
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function(err, res, ...)
-            local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
-            if string.match(file_name, "^%.env") == nil and string.match(file_name, "%.env$") == nil then
-              return on_publish_diagnostics(err, res, ...)
-            end
-          end,
+    opts = {
+      servers = {
+        bashls = {
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function(err, res, ...)
+              local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
+              if string.match(file_name, "^%.env") == nil and string.match(file_name, "%.env$") == nil then
+                return on_publish_diagnostics(err, res, ...)
+              end
+            end,
+          },
         },
-      })
-    end,
+        yamlls = {
+            format = {
+              enable = true,
+            },
+            hover = true,
+            completion = true,
+
+            customTags = {
+              "!fn",
+              "!And",
+              "!If",
+              "!Not",
+              "!Equals",
+              "!Or",
+              "!FindInMap sequence",
+              "!Base64",
+              "!Cidr",
+              "!Ref",
+              "!Ref Scalar",
+              "!Sub",
+              "!GetAtt",
+              "!GetAZs",
+              "!ImportValue",
+              "!Select",
+              "!Split",
+              "!Join sequence",
+            },
+        },
+      }
+    },
   },
 }
