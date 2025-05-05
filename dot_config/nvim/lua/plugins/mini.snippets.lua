50193d101554cc487ccf52fc56b797de89aea3b0
Sam Dickinson <sam.dickinson@reece.com.au>
2025-05-05 12:10:32 +1000

    Add .config/nvim/lua/plugins/mini.snippets.lua

---
+++ dot_config/nvim/lua/plugins/mini.snippets.lua
@@ -0,0 +1,10 @@
+return {
+  "echasnovski/mini.snippets",
+  opts = function(_, opts)
+    local snippets, config_path = require("mini.snippets"), vim.fn.stdpath("config")
+    opts.snippets = {
+      snippets.gen_loader.from_file(config_path .. "/snippets/global.json"),
+      snippets.gen_loader.from_lang(),
+    }
+  end,
+}
