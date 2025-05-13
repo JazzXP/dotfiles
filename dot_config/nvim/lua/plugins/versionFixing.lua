d482b460d51dc8b5dd3b7b2753d4f87be984bd26
Sam Dickinson <sam@sdickinson.com>
2025-05-13 22:38:29 +1000

    Merged tweaks

--- dot_config/nvim/lua/plugins/versionFixing.lua
+++ dot_config/nvim/lua/plugins/versionFixing.lua
@@ -3,6 +3,7 @@ return {
     "folke/trouble.nvim",
     version = "*",
   },
+  { "blink.cmp", version = "1.*" },
 
   { "mason-org/mason.nvim", version = "^1.0.0" },
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
