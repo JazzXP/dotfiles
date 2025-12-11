ae6625c259c2801d684455a8267f8576534f323e
Sam Dickinson <sam.dickinson@reece.com.au>
2025-12-11 14:56:44 +1100

    chore: updates

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- double ; in insert mode will automattically add a semicolon at the end of the line if there isn't one already
+vim.keymap.set("i", ";;", function()
+  local line = vim.api.nvim_get_current_line()
+  local trimmed = line:gsub("%s+$", "")
+
+  if trimmed:len() > 0 and string.sub(trimmed, -1) ~= ";" then
+    trimmed = trimmed .. ";"
+  end
+  vim.api.nvim_set_current_line(trimmed)
+end, { noremap = true })
--- dot_config/nvim/lua/plugins/vscode-diff.lua
+++ dot_config/nvim/lua/plugins/vscode-diff.lua
@@ -1,7 +1,7 @@
 return {
   "esmuellert/vscode-diff.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
-  keys = {
-    { "<leader>gD", "<Cmd>CodeDiff<cr>", desc = "VSCode Diff" },
-  },
+  -- keys = {
+  --   { "<leader>gD", "<Cmd>CodeDiff<cr>", desc = "VSCode Diff" },
+  -- },
 }
