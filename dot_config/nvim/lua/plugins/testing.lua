2bbf3e575acdca8b0a6ef2fa3217f8a81d10c9ec
Sam Dickinson <sam.dickinson@reece.com.au>
2026-01-16 14:33:26 +1100

    feat: move to service account

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -5,11 +5,16 @@ return {
     -- "nvim-neotest/nvim-nio",
   },
   opts = {
+    log_level = vim.log.levels.DEBUG,
     adapters = {
       ["neotest-vitest"] = {
         vitestCommand = "npx vitest run --coverage",
+        vitestConfigPath = vim.fn.getcwd() .. "/vitest.config.ts",
         filter_dir = function(name, rel_path, root)
           return name ~= "node_modules"
+        end,
+        cwd = function(path)
+          return vim.fn.getcwd()
         end,
       },
     },
