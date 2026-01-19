a66bdfe2d8f688b5e6c670fbf9edadb3b81365ef
Sam Dickinson <sam.dickinson@reece.com.au>
2026-01-19 13:48:21 +1100

    feat: change colors to Nord

---
+++ dot_config/nvim/lua/plugins/nord.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    opts = {
+      after_palette = function(palette)
+        local U = require("nordic.utils")
+        palette.bg_visual = U.blend(palette.orange.base, palette.bg, 0.15)
+        palette.fg_sidebar = U.blend(palette.fg, palette.bg, 0.3)
+      end,
+      swap_backgrounds = true,
+      cursorline = {
+        theme = "light",
+        blend = 0.2,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+}
