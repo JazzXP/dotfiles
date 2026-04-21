423df93f4fe6414919ce5261b2362f6a0735815c
Sam Dickinson <sam.dickinson@reece.com.au>
2026-04-21 10:15:42 +1000

    feat(nvim): update tailwind-dial

--- dot_config/nvim/lua/plugins/tailwindcss-dial.lua
+++ dot_config/nvim/lua/plugins/tailwindcss-dial.lua
@@ -2,6 +2,8 @@ return {
   "ruicsh/tailwindcss-dial.nvim",
   dependencies = { "monaqa/dial.nvim" },
   opts = {
+    -- filetypes = { "typescript" },
+    group = "typescript",
     -- group = "default", -- optional, defaults to "default"
   },
 }
