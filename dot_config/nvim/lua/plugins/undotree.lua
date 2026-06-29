63606972c1f2866fbf755768179fd0404be4fb25
Sam Dickinson <sam.dickinson@reece.com.au>
2026-06-30 09:45:32 +1000

    fix(nvim): icon for undotree in which-key

--- dot_config/nvim/lua/plugins/undotree.lua
+++ dot_config/nvim/lua/plugins/undotree.lua
@@ -1,14 +1,21 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
-  config = true,
-  keys = { -- load the plugin only when using it's keybinding:
-    {
-      "<leader>U",
-      function()
-        require("undotree").toggle()
-      end,
-      desc = "Undotree",
-    },
-  },
+  config = function()
+    require("undotree").setup()
+
+    -- Register keymap through which-key so icons work
+    local wk = require("which-key")
+
+    wk.add({
+      {
+        "<leader>U",
+        function()
+          require("undotree").toggle()
+        end,
+        desc = "Undotree",
+        icon = "",
+      },
+    })
+  end,
 }
