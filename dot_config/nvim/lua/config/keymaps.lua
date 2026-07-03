8fe63fe1e2d475fd8fe0e88c333d91389c1e4b25
Sam Dickinson <sam@sdickinson.com>
2026-07-03 23:34:57 +1000

    fix(neovim): headr navigation

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,33 @@ vim.keymap.set("i", ";;", function()
   end
   vim.api.nvim_set_current_line(trimmed)
 end, { noremap = true })
+
+local function nav(wincmd, dir)
+  local prev = vim.api.nvim_get_current_win()
+  vim.cmd("wincmd " .. wincmd)
+  if vim.api.nvim_get_current_win() ~= prev then
+    return
+  end
+
+  if vim.env.HERDR_PANE_ID and vim.env.HERDR_PANE_ID ~= "" then
+    local herdr = vim.env.HERDR_BIN_PATH or "herdr"
+    vim.fn.system({ herdr, "pane", "focus", "--direction", dir, "--current" })
+  elseif vim.env.TMUX and vim.env.TMUX ~= "" then
+    local tmux = { left = "Left", down = "Down", up = "Up", right = "Right" }
+    pcall(vim.cmd, "TmuxNavigate" .. tmux[dir])
+  end
+end
+
+-- Simply define them here. This will override LazyVim's defaults.
+vim.keymap.set("n", "<C-h>", function()
+  nav("h", "left")
+end, { desc = "Navigate left" })
+vim.keymap.set("n", "<C-j>", function()
+  nav("j", "down")
+end, { desc = "Navigate down" })
+vim.keymap.set("n", "<C-k>", function()
+  nav("k", "up")
+end, { desc = "Navigate up" })
+vim.keymap.set("n", "<C-l>", function()
+  nav("l", "right")
+end, { desc = "Navigate right" })
--- dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,10 +1,6 @@
 return {
   "christoomey/vim-tmux-navigator",
-  lazy = false,
   init = function()
     vim.g.tmux_navigator_no_mappings = 1
-  end,
-  config = function()
-    dofile(vim.fn.expand("~/.config/herdr/plugins/config/vim-herdr-navigation/editor/nvim.lua"))
   end,
 }
